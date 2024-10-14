d808c90543cd7c5dc0120d5233d59ff1d737d67c
Igor Sily <igorsily2@gmail.com>
2024-10-13 23:19:02 -0300

    arrumando erro de build

--- src/remotion/root.tsx
+++ src/remotion/root.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Composition } from "remotion";
-import RemotionVideo from "@/remotion/remotion-video";
+import RemotionVideo, { Video } from "@/remotion/remotion-video";
 
-export const RemotionRoot: React.FC = () => {
+export const RemotionRoot = () => {
+  const defaultProps = {} as Video;
+
   return (
-    <>
-      <Composition
-        id="Empty"
-        component={RemotionVideo}
-        durationInFrames={60}
-        fps={30}
-        width={1280}
-        height={720}
-      />
-    </>
+    <Composition
+      id="Empty"
+      component={RemotionVideo}
+      durationInFrames={60}
+      fps={30}
+      width={1280}
+      height={720}
+      defaultProps={defaultProps}
+    />
   );
 };
